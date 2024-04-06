a96999c574e127601c22ccdc98d7c230ae2e7002
juescoryisus <jesuseduardog204@gmail.com>
2024-04-06 16:17:21 -0600

    primer repositorio

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace situacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnAgregarProducto = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.txtProducto = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblNombre_Archivo = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAgregarProducto
+            // 
+            this.btnAgregarProducto.Location = new System.Drawing.Point(31, 56);
+            this.btnAgregarProducto.Name = "btnAgregarProducto";
+            this.btnAgregarProducto.Size = new System.Drawing.Size(96, 49);
+            this.btnAgregarProducto.TabIndex = 0;
+            this.btnAgregarProducto.Text = "Agregar";
+            this.btnAgregarProducto.UseVisualStyleBackColor = true;
+            this.btnAgregarProducto.Click += new System.EventHandler(this.btnAgregarProducto_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 167);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(424, 346);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(482, 82);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(441, 431);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // txtProducto
+            // 
+            this.txtProducto.Location = new System.Drawing.Point(228, 44);
+            this.txtProducto.Name = "txtProducto";
+            this.txtProducto.Size = new System.Drawing.Size(213, 26);
+            this.txtProducto.TabIndex = 5;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(228, 82);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(76, 26);
+            this.txtCantidad.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(142, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Producto:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label2.Location = new System.Drawing.Point(142, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Cantidad:\r\n";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "CSV",
+            "XML"});
+            this.comboBox1.Location = new System.Drawing.Point(336, 133);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(105, 28);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // lblNombre_Archivo
+            // 
+            this.lblNombre_Archivo.AutoSize = true;
+            this.lblNombre_Archivo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lblNombre_Archivo.Location = new System.Drawing.Point(46, 136);
+            this.lblNombre_Archivo.Name = "lblNombre_Archivo";
+            this.lblNombre_Archivo.Size = new System.Drawing.Size(145, 20);
+            this.lblNombre_Archivo.TabIndex = 10;
+            this.lblNombre_Archivo.Text = "Nombre de archivo:\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label3.Location = new System.Drawing.Point(262, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(214, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Seleccionar el tipo de archivo\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(949, 544);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblNombre_Archivo);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.txtProducto);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnAgregarProducto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregarProducto;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox txtProducto;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblNombre_Archivo;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace situacion
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, int> inventario = new Dictionary<string, int>();
+        private DataTable tabla_de_inventario = new DataTable();
+        private string fileName;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTabla_de_inventario();
+        }
+
+        private void InitializeTabla_de_inventario()
+        {
+            tabla_de_inventario.Columns.Add("Producto", typeof(string));
+            tabla_de_inventario.Columns.Add("Cantidad", typeof(int));
+
+            dataGridView1.DataSource = tabla_de_inventario;
+        }
+
+        private void Actualizar_tabla_de_indevntario()
+        {
+            tabla_de_inventario.Rows.Clear();
+            foreach (var item in inventario)
+            {
+                tabla_de_inventario.Rows.Add(item.Key, item.Value);
+            }
+        }
+
+        private void tabla()
+        {
+            chart1.Series.Clear();
+            chart1.ChartAreas[0].AxisX.Interval = 1;
+
+            Series series = new Series
+            {
+                Name = "Productos",
+                IsVisibleInLegend = true,
+                ChartType = SeriesChartType.Column
+            };
+
+            foreach (var item in inventario)
+            {
+                series.Points.AddXY(item.Key, item.Value);
+            }
+
+            chart1.Series.Add(series);
+        }
+        private void btnAgregarProducto_Click(object sender, EventArgs e)
+        {
+
+
+            string producto = txtProducto.Text.Trim();
+            if (producto == "")
+            {
+                MessageBox.Show("Por favor ingrese un nombre de producto.");
+                return;
+            }
+
+            int cantidad;
+            if (!int.TryParse(txtCantidad.Text, out cantidad))
+            {
+                MessageBox.Show("Por favor ingrese una cantidad válida.");
+                return;
+            }
+
+            if (inventario.ContainsKey(producto))
+            {
+                inventario[producto] += cantidad;
+            }
+            else
+            {
+                inventario.Add(producto, cantidad);
+            }
+
+            Actualizar_tabla_de_indevntario();
+            tabla();
+
+            // Guardar el archivo al agregar un producto
+            string formatoSeleccionado = comboBox1.SelectedItem.ToString();
+            switch (formatoSeleccionado)
+            {
+                case "CSV":
+                    GuardarComoCSV();
+                    break;
+                case "XML":
+                    GuardarComoXML();
+                    break;
+                default:
+                    MessageBox.Show("Formato de archivo no válido.");
+                    break;
+            }
+        }
+
+        private void GuardarComoCSV()
+        {
+            fileName = "inventario.csv";
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                StringBuilder csvContent = new StringBuilder();
+                csvContent.AppendLine("Producto,Cantidad");
+                foreach (var item in inventario)
+                {
+                    csvContent.AppendLine($"{item.Key},{item.Value}");
+                }
+                writer.Write(csvContent.ToString());
+            }
+            lblNombre_Archivo.Text = $"Datos guardados en: {fileName}";
+        }
+
+        private void GuardarComoXML()
+        {
+            fileName = "inventario.xml";
+            DataSet dataSet = new DataSet();
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("Producto");
+            dataTable.Columns.Add("Cantidad");
+
+            foreach (var item in inventario)
+            {
+                dataTable.Rows.Add(item.Key, item.Value);
+            }
+
+            dataSet.Tables.Add(dataTable);
+            dataSet.WriteXml(fileName);
+            lblNombre_Archivo.Text = $"Datos guardados en: {fileName}";
+        }
+    }
+
+}
+
+    
